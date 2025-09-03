83c8ba5571714a518bcc5f53110da353243b9b60
Devanand-PA <devanandpa721@gmail.com>
2025-09-03 16:36:47 +0530

    added a personal keybinds file. Now I'll make it skip the working tree

--- dwm/personal_keybinds.h
+++ dwm/personal_keybinds.h
@@ -1,1 +1,5 @@
 	{ ShiftMask,			XK_Print, 		spawn,          SHCMD("scrot --select - | xclip -sel clip -t image/png") },
+		{ 0,		                XF86XK_AudioPlay,				spawn,        	{.v = (const char*[]){"playerctl","play-pause",NULL} } },
+	{ 0,		                XF86XK_AudioPrev,				spawn,        	{.v = (const char*[]){"playerctl","previous",NULL} } },
+	{ 0,		                XF86XK_AudioNext,				spawn,        	{.v = (const char*[]){"playerctl","next",NULL} } },
+
