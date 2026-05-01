b5035d00e58f0c6b89fb443b8f2d8d3f063e8067
Devanand-PA <devanandpa721@gmail.com>
2026-05-02 01:53:26 +0530

    customized dmenu so it optionally reads from pywal

---
+++ dmenu/colors.h
@@ -0,0 +1,8 @@
+        colors[SchemeNorm][0] = "#bbbbbb";
+        colors[SchemeNorm][1] = "#222222";
+
+        colors[SchemeSel][0]  = "#eeeeee";
+        colors[SchemeSel][1]  = "#005577";
+
+        colors[SchemeOut][0]  = "#000000";
+        colors[SchemeOut][1]  = "#00ffff";
--- dmenu/config.h
+++ dmenu/config.h
@@ -1,21 +1,27 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
+
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 1000;                    /* minimum width when centered */
+static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
+
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xdd;     /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrains Mono:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
+static const char *colors[SchemeLast][2];
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+};
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
